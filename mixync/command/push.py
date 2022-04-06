ca4a9d03642b08acc76bd0b3aface596ce61c97b
fwcd <fwcdmail@gmail.com>
2022-04-06 02:35:31 +0200

    Copy track locations and relativize paths

--- mixync/command/push.py
+++ mixync/command/push.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import make_transient
+from pathlib import Path
 
 from mixync.context import Context
 from mixync.model.library import LibraryEntry
+from mixync.model.track_locations import TrackLocation
 
 def perform_push(ctx: Context):
     copy_library(ctx)
+    copy_track_locations(ctx)
 
 def copy_library(ctx: Context):
     entries = []
@@ -19,6 +22,32 @@ def copy_library(ctx: Context):
                 #       (this seems to be non-trivial with the high-level ORM API though)
                 if not portable_session.query(LibraryEntry).where(LibraryEntry.title == row.title, LibraryEntry.artist == row.artist).first():
                     entries.append(row)
+
             portable_session.add_all(entries)
             portable_session.commit()
+
     print(f'==> Copied {len(entries)} library entries')
+
+def copy_track_locations(ctx: Context):
+    entries = []
+    visited_locations = set()
+    with ctx.local_store.session() as local_session:
+        with ctx.portable_store.session() as portable_session:
+            for row in local_session.query(TrackLocation):
+                make_transient(row)
+                row.id = None
+
+                # Use relative paths in portable db
+                directory = Path(row.directory)
+                rel_location = Path(row.location).relative_to(directory.parent).as_posix()
+                row.directory = directory.name
+                row.location = rel_location
+
+                if not row.fs_deleted and rel_location not in visited_locations and not portable_session.query(TrackLocation).where(TrackLocation.location == rel_location).first():
+                    visited_locations.add(rel_location)
+                    entries.append(row)
+
+            portable_session.add_all(entries)
+            portable_session.commit()
+
+    print(f'==> Copied {len(entries)} track locations')
