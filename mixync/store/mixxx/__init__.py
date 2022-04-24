3f1b8523c4ebb2e8b8fcd2834a3d00d7dee78378
fwcd <fwcdmail@gmail.com>
2022-04-24 02:57:17 +0200

    Fix typo

--- mixync/store/mixxx/__init__.py
+++ mixync/store/mixxx/__init__.py
@@ -128,7 +128,7 @@ class MixxxStore(Store):
         location = Path(directory.location)
         if not location.parts:
             raise ValueError('Cannot absolutize a directory with an empty location path.')
-        matching_location = self._find_matching_directory(location.parts[0])
+        matching_location = self._find_matching_directory(location.parts[0], opts)
         if not confirm(f"Map '{directory.location}' to '{matching_location}'?", opts):
             sys.exit(0)
         new_directory.location = matching_location
