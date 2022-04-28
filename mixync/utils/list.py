c8b299709455793dcf2a84a1ac546092d4245949
fwcd <fwcdmail@gmail.com>
2022-04-28 18:29:55 +0200

    Add list compacting utilities

--- mixync/utils/list.py
+++ mixync/utils/list.py
@@ -1,6 +1,16 @@
-from typing import Optional, TypeVar
+from typing import Optional, TypeVar, Any
 
 T = TypeVar('T')
 
 def zip_or(xs: list[Optional[T]], ys: list[Optional[T]]) -> list[Optional[T]]:
     return [x or y for x, y in zip(xs, ys)]
+
+def compact(xs: list[Optional[T]]) -> list[T]:
+    return [x for x in xs if x]
+
+def uncompact(xs: list[T], pattern: list[Optional[Any]]) -> list[Optional[T]]:
+    it = iter(xs)
+    ys = []
+    for p in pattern:
+        ys.append(next(it) if p else None)
+    return ys
