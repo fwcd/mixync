7b9137e332d67b0bfae6e430ad95fc31401246f7
fwcd <fwcdmail@gmail.com>
2022-04-29 03:19:28 +0200

    Generate proper fake ids in debug store

--- mixync/store/debug.py
+++ mixync/store/debug.py
@@ -1,8 +1,13 @@
+from typing import TypeVar
+
 from mixync.model.crate import Crate
 from mixync.model.directory import Directory
 from mixync.model.playlist import Playlist
 from mixync.model.track import Track
 from mixync.store import Store
+from mixync.utils.typing import HasId
+
+T = TypeVar('T', bound='HasId')
 
 class DebugStore(Store):
     """A simple store that outputs updates to stdout for debugging."""
@@ -18,25 +23,36 @@ class DebugStore(Store):
             return DebugStore(compact=True)
         return None
     
+    def _fake_ids(self, values: list[T]) -> list[int]:
+        i = max([0] + [v.id for v in values if v.id]) + 1
+        new_ids = []
+        for value in values:
+            if value.id:
+                new_ids.append(value.id)
+            else:
+                new_ids.append(i)
+                i += 1
+        return new_ids
+    
     def update_tracks(self, tracks: list[Track]) -> list[int]:
         for track in tracks:
             print(track.name if self.compact else track)
-        return list(range(len(tracks)))
+        return self._fake_ids(tracks)
 
     def update_crates(self, crates: list[Crate]) -> list[int]:
         for crate in crates:
             print(crate.name if self.compact else crate)
-        return list(range(len(crates)))
+        return self._fake_ids(crates)
 
     def update_playlists(self, playlists: list[Playlist]) -> list[int]:
         for playlist in playlists:
             print(playlist.name if self.compact else playlist)
-        return list(range(len(playlists)))
+        return self._fake_ids(playlists)
 
     def update_directories(self, directories: list[Directory]) -> list[int]:
         for directory in directories:
             print(directory.location if self.compact else directory)
-        return list(range(len(directories)))
+        return self._fake_ids(directories)
 
     # Upload/download methods
 
